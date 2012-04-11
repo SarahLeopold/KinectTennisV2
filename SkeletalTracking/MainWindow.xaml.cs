487157aeb36ad4acbf787d3be14d3078d9e20f98
Sarah Leopold <srleopold@gmail.com>
2012-04-11 10:59:13 -0400

    This is a commit

--- SkeletalTracking/MainWindow.xaml.cs
+++ SkeletalTracking/MainWindow.xaml.cs
@@ -17,7 +17,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Kinect;
-using Coding4Fun.Kinect.Wpf; 
+using Coding4Fun.Kinect.Wpf;
+using ShapeGame.Utils;
+using ShapeGame;
+
 
 namespace SkeletalTracking
 {
@@ -35,15 +38,27 @@ namespace SkeletalTracking
         bool closing = false;
         const int skeletonCount = 6; 
         Skeleton[] allSkeletons = new Skeleton[skeletonCount];
+
+        //private Skeleton[] allSkeletons;
+        private readonly Dictionary<int, Player> players = new Dictionary<int, Player>();
+        private Rect playerBounds;
+        
         bool hit;
         int score = 0;
-        Random rdm;
+        Random rdm = new Random();
+
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
             hit = false;
             label1.Content=score;
+
+            playerBounds.X = 0;
+            this.playerBounds.Width = this.canvas1.ActualWidth*.7;
+            this.playerBounds.Y = this.canvas1.ActualHeight*.05 ;
+            this.playerBounds.Height = this.canvas1.ActualHeight ;
+
         }
 
         void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
@@ -103,8 +118,97 @@ namespace SkeletalTracking
                 return; 
             }
 
+            canvas1.Children.Clear();
+            foreach (var player in this.players)
+            {
+                player.Value.Draw(canvas1.Children);
+            }
+            
+            //label1.Content = "players size: " + players.Count;
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    int skeletonSlot = 0;
+
+                    if ((this.allSkeletons == null) || (this.allSkeletons.Length != skeletonFrame.SkeletonArrayLength))
+                    {
+                        this.allSkeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    }
+
+                    skeletonFrame.CopySkeletonDataTo(this.allSkeletons);
+
+                    foreach (Skeleton skeleton in this.allSkeletons)
+                    {
+                        if (SkeletonTrackingState.Tracked == skeleton.TrackingState)
+                        {
+                            Player player;
+                            if (this.players.ContainsKey(skeletonSlot))
+                            {
+                                player = this.players[skeletonSlot];
+                            }
+                            else
+                            {
+                                player = new Player(skeletonSlot);
+                                player.SetBounds(this.playerBounds);
+                                this.players.Add(skeletonSlot, player);
+                            }
+
+                            player.LastUpdated = DateTime.Now;
+
+                            // Update player's bone and joint positions
+                            if (skeleton.Joints.Count > 0)
+                            {
+                                player.IsAlive = true;
+                                // Head, hands, feet (hit testing happens in order here)
+                                player.UpdateJointPosition(skeleton.Joints, JointType.Head);
+                                player.UpdateJointPosition(skeleton.Joints, JointType.HandLeft);
+                                player.UpdateJointPosition(skeleton.Joints, JointType.HandRight);
+                                player.UpdateJointPosition(skeleton.Joints, JointType.FootLeft);
+                                player.UpdateJointPosition(skeleton.Joints, JointType.FootRight);
+
+                                // Hands and arms
+                                player.UpdateBonePosition(skeleton.Joints, JointType.HandRight, JointType.WristRight);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.WristRight, JointType.ElbowRight);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.ElbowRight, JointType.ShoulderRight);
+
+                                player.UpdateBonePosition(skeleton.Joints, JointType.HandLeft, JointType.WristLeft);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.WristLeft, JointType.ElbowLeft);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.ElbowLeft, JointType.ShoulderLeft);
+
+                                // Head and Shoulders
+                                player.UpdateBonePosition(skeleton.Joints, JointType.ShoulderCenter, JointType.Head);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.ShoulderLeft, JointType.ShoulderCenter);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.ShoulderCenter, JointType.ShoulderRight);
+
+                                // Legs
+                                player.UpdateBonePosition(skeleton.Joints, JointType.HipLeft, JointType.KneeLeft);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.KneeLeft, JointType.AnkleLeft);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.AnkleLeft, JointType.FootLeft);
+
+                                player.UpdateBonePosition(skeleton.Joints, JointType.HipRight, JointType.KneeRight);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.KneeRight, JointType.AnkleRight);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.AnkleRight, JointType.FootRight);
+
+                                player.UpdateBonePosition(skeleton.Joints, JointType.HipLeft, JointType.HipCenter);
+                                player.UpdateBonePosition(skeleton.Joints, JointType.HipCenter, JointType.HipRight);
+
+                                // Spine
+                                player.UpdateBonePosition(skeleton.Joints, JointType.HipCenter, JointType.ShoulderCenter);
+
+                                //label1.Content = player.ToString();
+
+                            }
+                        }
+
+                        skeletonSlot++;
 
 
+                    }
+                }                       
+
+            }
             //set scaled position
             //ScalePosition(headImage, first.Joints[JointType.Head]);
             //ScalePosition(leftEllipse, first.Joints[JointType.HandLeft]);
@@ -135,8 +239,8 @@ namespace SkeletalTracking
                 //double actualDistanceLeft = Math.Sqrt(Math.Pow(ellipseCenterX - leftCenterX, 2) + Math.Pow(ellipseCenterY - leftCenterY, 2));
 
                 //right hand
-                double rightCenterX = Canvas.GetLeft(rightEllipse) - rightEllipse.Width / 2;
-                double rightCenterY = Canvas.GetTop(rightEllipse) - rightEllipse.Height / 2;
+                double rightCenterX = Canvas.GetLeft(rightEllipse) + rightEllipse.Width / 2;
+                double rightCenterY = Canvas.GetTop(rightEllipse) + rightEllipse.Height / 2;
 
                 double upperBoundRight = ellipse1.Width / 2 + rightEllipse.Width / 2;
                 double actualDistanceRight = Math.Sqrt(Math.Pow(ellipseCenterX - rightCenterX, 2) + Math.Pow(ellipseCenterY - rightCenterY, 2));
@@ -145,7 +249,23 @@ namespace SkeletalTracking
                 {
                     //increment score label
                     score++;
-                    label1.Content = score;
+                    label1.Content = score; //how to make this increment just once per hit.
+                    /*while(ellipse1.Width>1 && ellipse1.Height>1){ //ball moving away from player  needs to be in all frames ready, hitable variable
+                        ellipse1.Width--;
+                        ellipse1.Height--;
+                    }
+
+                    int x = rdm.Next((int)(MainCanvas.Width - 50));
+                    int y = rdm.Next((int)((MainCanvas.Height/2) - 50));
+                    //set ball coordinates
+                    Canvas.SetLeft(ellipse1, x);
+                    Canvas.SetTop(ellipse1, y);
+                    
+                    //should this be moved elsewhere?
+                    while(ellipse1.Width<50 && ellipse1.Height<50){ //ball moving toward player
+                        ellipse1.Width++;
+                        ellipse1.Height++;
+                    }*/
                     //set hit to true
                     //hit = true;
                 }
@@ -262,7 +382,6 @@ namespace SkeletalTracking
             Canvas.SetTop(element, scaledJoint.Position.Y); 
             
         }
-
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
